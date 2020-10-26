da7cf8ab995575c01ff0063e33e88341de9e9338
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-26 13:03:52 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    fileName = input("Name of the file:")
+    search = input("Search for:")
+    try:
+      with open(fileName,'r') as f:
+        lines = f.read().splitlines()
+        if(search in lines):
+          print("Found!")
+        else:
+          print("Not found.")
+    except:
+      print("Reading the file %s failed."%fileName)
 
 if __name__ == '__main__':
     main()
